849fa14581338f2e55364e3a3023e0193f1da66b
Triptea <69861898+Triptea@users.noreply.github.com>
2021-05-12 00:42:55 +0530

    first commit
    initial commit

---
+++ Assets/Flying_Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flying_Ball : MonoBehaviour
+{
+    public float horizontalSpeed;
+    public float verticalSpeed;
+    public float aptitude;
+
+    public Vector3 tempPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tempPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        tempPosition.x += horizontalSpeed;
+        tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * aptitude;
+        transform.position = tempPosition;
+    }
+}
